1e2221d2871b2e63e9eb8030fc1591488b9a8114
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-11-24 20:44:20 -0800

    Create 2018-11-24_naive-bayes-for-sentiment-analysis.R

---
+++ src/2018-11-24_naive-bayes-for-sentiment-analysis.R
@@ -0,0 +1,72 @@
+
+#**************************************************
+# Naive Bayes Classification for Sentiment Analysis 
+# of Movie Reviews
+# 2018-11-24 
+#**************************************************
+
+library(here)
+library(tm)
+library(RTextTools)
+library(e1071)
+library(dplyr)
+library(caret)
+# Library for parallel processing
+# library(doMC)
+# registerDoMC(cores=detectCores())
+
+
+
+# Reference: https://rpubs.com/cen0te/naivebayes-sentimentpolarity 
+
+
+# read data: 
+df1.reviews <- read_data(here("data", 
+                           "movie-pang02.csv"))
+glimpse(df1.reviews)
+
+# randomize data: 
+set.seed(1)
+df1.reviews <- df1.reviews[sample(nrow(df1.reviews)), ]
+df1.reviews <- df1.reviews[sample(nrow(df1.reviews)), ]
+glimpse(df1.reviews)
+
+
+# Convert the 'class' variable from character to factor.
+df1.reviews$class <- as.factor(df1.reviews$class)
+
+
+# Bag of words tokenization -----
+# ?Corpus
+corpus <- Corpus(VectorSource(df1.reviews$text))
+
+# str(corpus, max.level = 1)  # not useful
+
+
+# Inspect the corpus: --------
+corpus
+## <<VCorpus>>
+## Metadata:  corpus specific: 0, document level (indexed): 0
+## Content:  documents: 2000
+inspect(corpus[1:3])
+
+corpus[[2]]
+
+
+
+# data cleanup: --------
+corpus.clean <- 
+    corpus %>% 
+    tm_map(content_transformer(tolower)) %>% 
+    tm_map(removePunctuation) %>% 
+    tm_map(removeNumbers) %>% 
+    tm_map(removeWords, 
+           stopwords(kind = "en")) %>% 
+    tm_map(stripWhitespace)
+
+inspect(corpus.clean[1:3])
+
+
+# matrix representation of bag of words: 
+
+
